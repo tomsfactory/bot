5a5c154ad7ec7abc6594863e9e17e6778ccd58d3
Tom <contact@jsdevtom.com>
2024-11-29 22:01:18 +0000

    Use single strings

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # This is the monorepo for Tom's future SaaS Empire.
 
 ## Getting started
+
 Run `./scripts/add-pre-commit.sh` to make sure you push decent code to remote.
--- bot/mod_test.ts
+++ bot/mod_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "@std/assert";
-import {add} from '@tomsfactory/bot';
+import { assertEquals } from '@std/assert';
+import { add } from '@tomsfactory/bot';
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
